4f89d4259c1b34d82114687c139aee7a887d2882
Mikhail Kozar <65019032+m1Ck1-pypy@users.noreply.github.com>
2024-12-29 11:47:54 +0300

    add dropdown zone upload file (#5)

--- frontend/src/screens/Upload/Upload.tsx
+++ frontend/src/screens/Upload/Upload.tsx
@@ -1,9 +1,10 @@
 import { Text } from '@gravity-ui/uikit';
 import { Container } from './styles/Container';
 import { Root } from './styles/Root';
-import { useRef, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import { Wrapper } from './styles/Wrapper';
 import UploadImage from './UploadImage/UploadImage';
+import { useDropzone } from 'react-dropzone';
 
 const Upload = () => {
   const fileRef = useRef<HTMLInputElement>(null);
@@ -22,20 +23,67 @@ const Upload = () => {
     }
   };
 
+  const onDrop = useCallback(async (acceptedFiles: File[]) => {
+    const selectedFile = acceptedFiles[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+    }
+
+    // acceptedFiles.forEach((file) => {
+    //   const reader = new FileReader();
+
+    //   reader.onabort = () => console.log('file reading was aborted');
+    //   reader.onerror = () => console.log('file reading has failed');
+    //   reader.onload = () => {
+    //     // Do whatever you want with the file contents
+    //     const binaryStr = reader.result;
+    //     console.log(binaryStr);
+    //   };
+    //   reader.readAsArrayBuffer(file);
+    // });
+    // try {
+    // for (const file of acceptedFiles) {
+    // const formData = new FormData();
+    // formData.append('file', file);
+
+    // const response = await axios.post('http://localhost:3000/upload', formData, {
+    //   headers: {
+    //     'Content-Type': 'multipart/form-data',
+    //   },
+    // });
+    // }
+    // } catch (error) {
+    //   console.error('Error uploading file:', error);
+    // }
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const props = {
+    ...getRootProps(),
+    onClick: handleUploadClick,
+  };
+
   return (
     <Root>
       {!file ? (
         <Wrapper>
-          <Container onClick={handleUploadClick}>
-            <Text variant='display-1'>Нажмите для загрузки изображения</Text>
-            <Text variant='display-1'>или</Text>
-            <Text variant='display-1'>Перетащите изображение в поле</Text>
+          <Container {...props}>
+            {!isDragActive ? (
+              <>
+                <Text variant='display-1'>Нажмите для загрузки изображения</Text>
+                <Text variant='display-1'>или</Text>
+                <Text variant='display-1'>Перетащите изображение в поле</Text>
+              </>
+            ) : (
+              <Text variant='display-1'>Отпустите изображение</Text>
+            )}
           </Container>
         </Wrapper>
       ) : (
         <UploadImage file={file} onUploadFile={handleUploadClick} onClose={() => setFile(null)} />
       )}
-      <input ref={fileRef} type='file' onChange={handleFileChange} accept='image/*' hidden />
+      <input {...getInputProps()} ref={fileRef} type='file' onChange={handleFileChange} accept='image/*' hidden />
     </Root>
   );
 };
