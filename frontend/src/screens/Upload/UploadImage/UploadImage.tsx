524a3dbd2cd7872b6679748440c68100591d1fa4
Michael Kozar <kozarmisha@mail.ru>
2024-12-29 11:47:09 +0300

    add dropdown zone upload file

--- frontend/src/screens/Upload/UploadImage/UploadImage.tsx
+++ frontend/src/screens/Upload/UploadImage/UploadImage.tsx
@@ -4,7 +4,7 @@ import { useRef } from 'react';
 import { Button, Icon, Tooltip } from '@gravity-ui/uikit';
 import { Actions } from './styles/Actions';
 
-import { ArrowsRotateLeft, FloppyDisk } from '@gravity-ui/icons';
+import { ArrowsRotateLeft, FloppyDisk, Xmark } from '@gravity-ui/icons';
 import { useGalleryStore } from '@/utils/store/gallery';
 import { useNavigate } from 'react-router-dom';
 
@@ -29,6 +29,11 @@ const UploadImage = ({ file, onUploadFile, onClose }: Props) => {
     <ImageWrapper>
       <Image ref={ref} image={URL.createObjectURL(file)} alt='Uploaded' />
       <Actions>
+        <Tooltip content='Удалить' placement='bottom' openDelay={300}>
+          <Button view='outlined-danger' size='l' onClick={onClose}>
+            <Icon data={Xmark} size={20} />
+          </Button>
+        </Tooltip>
         <Tooltip content='Изменить' placement='bottom' openDelay={300}>
           <Button view='outlined-info' size='l' onClick={onUploadFile}>
             <Icon data={ArrowsRotateLeft} size={20} />
